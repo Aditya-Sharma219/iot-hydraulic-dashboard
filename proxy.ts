9dc86926563eae82e12b583a6dc09c1561b20a27
Aditya <adityash219@gmail.com>
2025-11-24 00:53:05 +0530

    updated proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,25 +1,12 @@
-// middleware.ts
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-  "/",                     // homepage public
-  "/sign-in(.*)",          // sign in page
-  "/sign-up(.*)",          // sign up page
-  "/api/public/(.*)",      // public APIs
-]);
-
-export default clerkMiddleware((auth, req) => {
-  // for public routes → allow, do nothing
-  if (isPublicRoute(req)) return;
-
-  // for private routes but not logged in → DO NOT REDIRECT here
-  // simply let the request go through; page will handle authentication
-  return;
-});
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
-};
+}
